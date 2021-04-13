29d14c7008b6e5d7a5d2d7f52ef1c4616bf0c214
Nils <nils.bochow@uit.no>
2021-04-13 15:14:57 +0200

    ADDED: run without noise instead gaussian filter
    Now the run without noise is used to get the residual time series instead of a gaussian filter.

--- analysis/ensemble_analysis.jl
+++ analysis/ensemble_analysis.jl
@@ -1,5 +1,4 @@
 module EnsembleAnalysis 
-    export mean_quantiles_indicator
     using PyCall
     using JLD2
     using DifferentialEquations
@@ -8,12 +7,14 @@ module EnsembleAnalysis
     include("../Plotting_functions.jl")
     using .North
     using .PlottingFunctions
-    
+    using FileIO
+
     directory = @__DIR__
     directory = directory[1:end-8]
-    JLD2.@load directory * "/output/" * "ensemble_sol_100r.jld" sol
+    sol_no_noise = load(directory * "/output/" * "temp_no_noise10r.jld", "sol")
+    JLD2.@load directory * "/output/" * "ensemble_sol_noalbedo10r.jld" sol
 
-
+   
     function rolling_mean(array, window_size)
         """
         Function to calculate rolling mean using pandas from Python.
@@ -39,13 +40,9 @@ module EnsembleAnalysis
         from scipy import sparse
         from scipy.ndimage import gaussian_filter1d
 
-        #def month_array(month, array): 
-        #    return array[np.arange(month, array.size, 12)]
-
         def rolling_statistics(function_string, variable_month, window_size):
             window_size = int(window_size/12)
-            #variable_month = month_array(month, variable)
-            detrended_month = variable_month - gaussian_filter1d(variable_month, sigma =  10, mode = "nearest")
+            detrended_month = variable_month#variable_month - gaussian_filter1d(variable_month, sigma =  10, mode = "nearest")
             if function_string == "autocorrelation":
                 #detrended_month = detrended_month[0:250]
                 s = pd.Series(detrended_month)
@@ -63,25 +60,27 @@ module EnsembleAnalysis
 
 
     function analysis()
-        month = 9
+        month = 3
         window_size = 12*50
         glob_temp = Array{Float32}(undef, size(sol, 2), size(sol, 3))
         sia = Array{Float32}(undef, size(sol, 2), size(sol, 3))
+        sia_detrended = similar(sia)
         #sia_rolling = Array{Float32}(undef, size(sol, 2), size(sol, 3))
         glob_temp_year = Array{Float32}(undef, convert(Int16, floor(size(sol, 2)/12)), size(sol, 3))
         for i = 1:1:(size(sol, 3))
-
+            #sol_detrended[:,:,i] = sol[:,:,i] .- sol_no_noise
             glob_temp[:, i] = North.calculate_global_average_T(sol[:,:,i])
             glob_temp_year[:, i] = mean(reshape(glob_temp[1:end-1, i], 12, :), dims = 1)
             sia[:, i] = North.calculate_SIA(sol[:,:,i])
+            sia_detrended[:, i] = sia[:, i] - North.calculate_SIA(sol_no_noise[:,:])
             #sia_rolling[:, i] .= rolling_mean(sia[:, i], 12)
             #sia_var[:, i] = variance_autocorrelation("autocorrelation", sia[:, i], window_size, month)
         end
-        sia_month = month_array(sia, month)
+        sia_month = month_array(sia_detrended, month)
 
         quantiles_var, mean_sia_var = mean_quantiles_indicator(sia_month, "var", window_size, month)
         quantiles_ac, mean_sia_ac= mean_quantiles_indicator(sia_month, "ac", window_size, month)
-        quantiles_sia, mean_sia= mean_quantiles_indicator(sia_month, "none", window_size, month)
+        quantiles_sia, mean_sia= mean_quantiles_indicator(month_array(sia, month), "none", window_size, month)
         PlottingFunctions.confidence_plot(quantiles_var, convert(Int8, window_size/12), mean_sia_var, 25, "Variance [km²]", "variance_month$(month)")
         PlottingFunctions.confidence_plot(quantiles_ac, convert(Int8, window_size/12), mean_sia_ac, 25, "AC(1)", "ac1_month$(month)")
         PlottingFunctions.confidence_plot(quantiles_sia, 0, mean_sia, 25, "SIA [km²]", "sia_month$(month)")
